8679b85eea54269ce8139440d8125252c53dd606
Shofa <108876019+Ahmed-Elgendy25@users.noreply.github.com>
2024-05-05 04:42:41 +0300

    updated QuestionComponent+ExamScreen

--- app/(components)/QuestionsComponent.tsx
+++ app/(components)/QuestionsComponent.tsx
@@ -1,33 +1,202 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import { RadioButton } from 'react-native-paper';
-import { OptionsTyped } from './type';
 
-const QuestionsComponent = ({ options }: { options: OptionsTyped }) => {
-  const [selectedValue, setSelectedValue] = useState<string | undefined>(
-    undefined
+interface Question {
+  question: string;
+  options: string[];
+  answer: string;
+}
+
+interface Props {
+  sampleQuestions?: Question[];
+  duration?: number;
+  setScore: React.Dispatch<React.SetStateAction<number | null>>;
+  timeLeft: number;
+  setTimeLeft: React.Dispatch<React.SetStateAction<number>>;
+  submitted: boolean;
+  setSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const QuestionsComponent: React.FC<Props> = ({
+  sampleQuestions = [],
+  duration = 0,
+  setScore,
+  timeLeft,
+  setTimeLeft,
+  submitted,
+  setSubmitted,
+}) => {
+  const [selectedOptionIndices, setSelectedOptionIndices] = useState<number[]>(
+    new Array(sampleQuestions.length).fill(null)
   );
 
-  const handleValueChange = (value: any) => {
-    setSelectedValue(value);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime === 0) {
+          clearInterval(interval);
+          handleSubmit();
+          return prevTime;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (submitted) {
+      const calculatedScore = calculateDegree();
+      setScore(calculatedScore);
+    }
+  }, [submitted]);
+
+  const handleOptionPress = (questionIndex: number, optionIndex: number) => {
+    if (submitted || timeLeft === 0) return;
+    const newSelectedOptionIndices = [...selectedOptionIndices];
+    newSelectedOptionIndices[questionIndex] = optionIndex;
+    setSelectedOptionIndices(newSelectedOptionIndices);
+  };
+
+  const calculateDegree = () => {
+    return sampleQuestions.reduce((correctAnswersCount, question, index) => {
+      const userChoiceIndex = selectedOptionIndices[index];
+      if (
+        userChoiceIndex !== null &&
+        question.answer === question.options[userChoiceIndex]
+      ) {
+        correctAnswersCount++;
+      }
+      return correctAnswersCount;
+    }, 0);
+  };
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  const renderQuestions = () => {
+    return sampleQuestions.map((question, index) => (
+      <View key={index} style={styles.questionContainer}>
+        <Text style={styles.question}>{`Q${index + 1}: ${
+          question.question
+        }`}</Text>
+        {question.options.map((option, optionIndex) => (
+          <View key={optionIndex} style={styles.option}>
+            <RadioButton
+              value={String(optionIndex)}
+              status={
+                selectedOptionIndices[index] === optionIndex
+                  ? 'checked'
+                  : 'unchecked'
+              }
+              onPress={() => handleOptionPress(index, optionIndex)}
+              disabled={submitted || timeLeft === 0}
+            />
+            <Text style={styles.optionText}>{option}</Text>
+          </View>
+        ))}
+      </View>
+    ));
   };
 
   return (
-    <>
-      {options.map((option, index) => {
-        return (
-          <RadioButton.Group
-            value={selectedValue}
-            onValueChange={handleValueChange}
-            name="myRadioGroup"
-            accessibilityLabel="Pick your Answer"
-            key={index}
-          >
-            <RadioButton.Item value={option} label={option} />
-          </RadioButton.Group>
-        );
-      })}
-    </>
+    <ScrollView contentContainerStyle={styles.container}>
+      {renderQuestions()}
+      {!submitted && (
+        <TouchableOpacity onPress={handleSubmit} style={styles.submitButton}>
+          <Text style={styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+      )}
+    </ScrollView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: 'lightgray',
+  },
+  questionContainer: {
+    marginBottom: 20,
+  },
+  question: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'lightgray',
+  },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginLeft: 15,
+  },
+  optionText: {
+    width: '100%',
+    fontSize: 16,
+    marginLeft: 10,
+  },
+  submitButton: {
+    backgroundColor: '#F19A1A',
+    width: '65%',
+    alignSelf: 'center',
+    padding: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+    margin: 20,
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
 export default QuestionsComponent;
+
+// import React, { useState } from 'react';
+// import { RadioButton } from 'react-native-paper';
+// import { OptionsTyped } from './type';
+
+// const QuestionsComponent = ({ options }: { options: OptionsTyped }) => {
+//   const [selectedValue, setSelectedValue] = useState<string | undefined>(
+//     undefined
+//   );
+
+//   const handleValueChange = (value: any) => {
+//     setSelectedValue(value);
+//   };
+
+//   return (
+//     <>
+//       {options.map((option, index) => {
+//         return (
+//           <RadioButton.Group
+//             value={selectedValue}
+//             onValueChange={handleValueChange}
+//             name="myRadioGroup"
+//             accessibilityLabel="Pick your Answer"
+//             key={index}
+//           >
+//             <RadioButton.Item value={option} label={option} />
+//           </RadioButton.Group>
+//         );
+//       })}
+//     </>
+//   );
+// };
+
+// export default QuestionsComponent;
--- app/ExamScreen/index.tsx
+++ app/ExamScreen/index.tsx
@@ -1,78 +1,212 @@
-import { Box, Heading, Radio, Text } from 'native-base';
-import React, { useEffect, useState } from 'react';
-import { mcqQuestions } from '../(components)/questions';
-import { router, useNavigation } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { ScrollView } from 'react-native';
-// import QuestionsComponent from '../(components)/QuestionsComponent';
-import { Button } from 'react-native-paper';
-import QuestionsComponent from '../(components)/QuestionsComponent';
-import CountDown from 'react-native-countdown-component';
+import React, { useState } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { Text } from 'react-native-paper';
+import QuestionsComponent from '../(components)/QuestionsComponent'; // Assuming the correct path to QuestionsScreen
 
-const index = () => {
-  const navigation = useNavigation();
-  useEffect(() => {
-    navigation.setOptions({ headerShown: false });
-  }, [navigation]);
+interface Question {
+  question: string;
+  options: string[];
+  answer: string;
+}
+
+const QuizScreen: React.FC = () => {
+  // Sample questions data
+  const sampleQuestions: Question[] = [
+    {
+      question: 'What is the capital of France?',
+      options: ['lorem', 'Paris', 'Berlin', 'Madrid'],
+      answer: 'Paris',
+    },
+    {
+      question: 'Who wrote "To Kill a Mockingbird"?',
+      options: ['Harper Lee', 'Stephen King', 'J.K. Rowling', 'Mark Twain'],
+      answer: 'Harper Lee',
+    },
+    {
+      question: 'What is the powerhouse of the cell?',
+      options: ['Nucleus', 'Mitochondria', 'Ribosome', 'Endoplasmic Reticulum'],
+      answer: 'Mitochondria',
+    },
+    {
+      question: 'What is the chemical symbol for water?',
+      options: ['H2O', 'CO2', 'NaCl', 'O2'],
+      answer: 'H2O',
+    },
+    // Add more questions here...
+  ];
+
+  // State variables
+  const [totalDuration, setTotalDuration] = useState<number>(10); // Default duration of 10 minutes
+  const [timeLeft, setTimeLeft] = useState<number>(totalDuration * 60); // Time left in seconds
+  const [score, setScore] = useState<number | null>(null);
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
   return (
-    <SafeAreaView>
-      <Box
-        w={'full'}
-        pt={10}
-        pb={2}
-        position={'absolute'}
-        left={0}
-        top={0}
-        zIndex={50}
-        bgColor={'#F19A1A'}
-      >
-        <CountDown
-          size={20}
-          until={10}
-          onFinish={() => alert('Finished')}
-          digitStyle={{
-            backgroundColor: '#FFF',
-          }}
-          digitTxtStyle={{ color: '#F19A1A' }}
-          timeLabelStyle={{ color: 'red', fontWeight: 'bold' }}
-          separatorStyle={{ color: '#F19A1A' }}
-          timeToShow={['H', 'M', 'S']}
-          timeLabels={{ m: '', s: '' }} // Provide valid values for the timeLabels prop
-          showSeparator
+    <View style={styles.container}>
+      {/* Header section */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Quiz Title</Text>
+        <Text style={styles.description}>
+          Description of the quiz goes here.
+        </Text>
+        <Text
+          style={styles.duration}
+        >{`Duration: ${totalDuration} minute`}</Text>
+        {/* Display time left and score */}
+        <View style={styles.score}>
+          <Text style={styles.scoreText}>
+            {submitted
+              ? 'Time Left: Finished'
+              : `Time Left: ${Math.floor(timeLeft / 60)}:${(
+                  '0' +
+                  (timeLeft % 60)
+                ).slice(-2)}`}
+          </Text>
+          <Text style={styles.scoreText}>
+            {`Score: ${
+              score !== null ? `${score}/${sampleQuestions.length}` : ''
+            }`}
+          </Text>
+        </View>
+      </View>
+      {/* Questions component section */}
+      <ScrollView contentContainerStyle={styles.form}>
+        <QuestionsComponent
+          sampleQuestions={sampleQuestions}
+          duration={totalDuration}
+          setScore={setScore}
+          timeLeft={timeLeft}
+          setTimeLeft={setTimeLeft}
+          submitted={submitted}
+          setSubmitted={setSubmitted}
         />
-      </Box>
-      <ScrollView
-        style={{
-          marginTop: 50,
-        }}
-      >
-        {mcqQuestions.map((question, i) => {
-          const { options } = question;
-          return (
-            <Box key={i} bgColor={'#F19A1A'}>
-              <Heading p={4} size={'xl'} color={'white'}>
-                Q{i + 1}
-              </Heading>
-              <Box p={3} backgroundColor={'white'}>
-                <Text fontWeight={'semibold'} fontSize={'2xl'} mb={2} p={3}>
-                  {question.question}
-                </Text>
-                <QuestionsComponent options={options} />
-              </Box>
-            </Box>
-          );
-        })}
-        <Button
-          textColor="white"
-          style={{ marginVertical: 10, padding: 5, backgroundColor: '#5f4bdf' }}
-          onPress={() => router.navigate('/')}
-        >
-          Submit
-        </Button>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
-export default index;
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+  },
+  header: {
+    paddingTop: 40,
+    padding: 20,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    backgroundColor: '#F19A1A',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#FFF',
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#FFF',
+  },
+  duration: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#FFF',
+  },
+  form: {
+    flexGrow: 1,
+    paddingBottom: 20,
+    marginBottom: 20,
+  },
+  score: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  scoreText: {
+    color: 'white',
+    width: 150,
+  },
+});
+
+export default QuizScreen;
+
+// import { Box, Heading, Radio, Text } from 'native-base';
+// import React, { useEffect, useState } from 'react';
+// import { mcqQuestions } from '../(components)/questions';
+// import { router, useNavigation } from 'expo-router';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { ScrollView } from 'react-native';
+// // import QuestionsComponent from '../(components)/QuestionsComponent';
+// import { Button } from 'react-native-paper';
+// import QuestionsComponent from '../(components)/QuestionsComponent';
+// import CountDown from 'react-native-countdown-component';
+
+// const index = () => {
+//   const navigation = useNavigation();
+//   useEffect(() => {
+//     navigation.setOptions({ headerShown: false });
+//   }, [navigation]);
+
+//   return (
+//     <SafeAreaView>
+//       <Box
+//         w={'full'}
+//         pt={10}
+//         pb={2}
+//         position={'absolute'}
+//         left={0}
+//         top={0}
+//         zIndex={50}
+//         bgColor={'#F19A1A'}
+//       >
+//         <CountDown
+//           size={20}
+//           until={10}
+//           onFinish={() => alert('Finished')}
+//           digitStyle={{
+//             backgroundColor: '#FFF',
+//           }}
+//           digitTxtStyle={{ color: '#F19A1A' }}
+//           timeLabelStyle={{ color: 'red', fontWeight: 'bold' }}
+//           separatorStyle={{ color: '#F19A1A' }}
+//           timeToShow={['H', 'M', 'S']}
+//           timeLabels={{ m: '', s: '' }} // Provide valid values for the timeLabels prop
+//           showSeparator
+//         />
+//       </Box>
+//       <ScrollView
+//         style={{
+//           marginTop: 50,
+//         }}
+//       >
+//         {mcqQuestions.map((question, i) => {
+//           const { options } = question;
+//           return (
+//             <Box key={i} bgColor={'#F19A1A'}>
+//               <Heading p={4} size={'xl'} color={'white'}>
+//                 Q{i + 1}
+//               </Heading>
+//               <Box p={3} backgroundColor={'white'}>
+//                 <Text fontWeight={'semibold'} fontSize={'2xl'} mb={2} p={3}>
+//                   {question.question}
+//                 </Text>
+//                 <QuestionsComponent options={options} />
+//               </Box>
+//             </Box>
+//           );
+//         })}
+//         <Button
+//           textColor="white"
+//           style={{ marginVertical: 10, padding: 5, backgroundColor: '#5f4bdf' }}
+//           onPress={() => router.navigate('/')}
+//         >
+//           Submit
+//         </Button>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default index;
